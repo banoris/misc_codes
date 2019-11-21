38a1e22e6cd4f620be461232d0a968f6cd933d00
banoris <banoris@protonmail.com>
2019-11-21 21:59:24 +0100

    Cpp: containers, custom hashers

--- cpp/container_practice.cc
+++ cpp/container_practice.cc
@@ -13,63 +13,110 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	hash<long> hasher_long;
-	size_t hashcode_42 = hasher_long(42);
-	assert(hashcode_42 == hasher_long(42));
+    hash<long> hasher_long;
+    size_t hashcode_42 = hasher_long(42);
+    assert(hashcode_42 == hasher_long(42));
 
-	// Don't confuse with the double parentheses.
-	// First one is for instantiating the hash object
-	//   - unnamed/anonymous object instantiation?
-	// Second is calling the operator() --> recall functor
-	assert(hash<long>()(42) == hasher_long(42));
+    // Don't confuse with the double parentheses.
+    // First one is for instantiating the hash object
+    //   - unnamed/anonymous object instantiation?
+    // Second is calling the operator() --> recall functor
+    assert(hash<long>()(42) == hasher_long(42));
 
 
-	// ------------------------------------------------- //
-	// Unorder set practice
-	// ------------------------------------------------- //
+    // ------------------------------------------------- //
+    // Unordered set practice
+    // ------------------------------------------------- //
 
-	// https://thispointer.com/how-to-initialize-an-unordered_set-in-c11/
-	// You did some mods on () vs {}. Opinionated guide: stick to braced init
+    // https://thispointer.com/how-to-initialize-an-unordered_set-in-c11/
+    // You did some mods on () vs {}. Opinionated guide: stick to braced init
 
-	// 1. Create unordered_set from plain array
-	int arr[] = { 2, 4, 6, 1, 3, 6, 8, 3, 2 };
+    // 1. Create unordered_set from plain array
+    int arr[] = { 2, 4, 6, 1, 3, 6, 8, 3, 2 };
 
-	// 2. Create an unordered set and initialize it with the array
-	//   Use parentheses ctor with (arr_var, sizeof(arr_var) / sizeof(arr_type))
-	// Set will contain only unique elements -- IMPORTANT!
-	std::unordered_set<int> setOfNum(arr, arr + sizeof(arr) / sizeof(int));
-	std::unordered_set<int> my_setOfNum{arr, arr + sizeof(arr) / sizeof(int)};
+    // 2. Create an unordered set and initialize it with the array
+    //   Use parentheses ctor with (arr_var, sizeof(arr_var) / sizeof(arr_type))
+    // Set will contain only unique elements -- IMPORTANT!
+    std::unordered_set<int> setOfNum(arr, arr + sizeof(arr) / sizeof(int));
+    std::unordered_set<int> my_setOfNum{arr, arr + sizeof(arr) / sizeof(int)};
 
-	// Iterate over the set and display contents
-	for (int val : my_setOfNum)
-		std::cout << val << "  ";
-	std::cout << std::endl;
+    // Iterate over the set and display contents
+    for (int val : my_setOfNum)
+        std::cout << val << "  ";
+    std::cout << std::endl;
 
-	// Create an unordered set and initialize it initializer_list
-	//   TODO: study initializer_list (╯°□°）╯︵ ┻━┻)
-	//     - Do we need to init with parentheses + braces? It works if remove '()'
-	//       - Yup, removed '()' and everything seems to work fine
-	std::unordered_set<int> setOfNum2( { 1, 2, 3, 1, 3, 4, 2 });
-	std::unordered_set<int> my_setOfNum2{ 1, 2, 3, 1, 3, 4, 2 };
+    // Create an unordered set and initialize it initializer_list
+    //   TODO: study initializer_list (╯°□°）╯︵ ┻━┻)
+    //     - Do we need to init with parentheses + braces? It works if remove '()'
+    //       - Yup, removed '()' and everything seems to work fine
+    std::unordered_set<int> setOfNum2( { 1, 2, 3, 1, 3, 4, 2 });
+    std::unordered_set<int> my_setOfNum2{ 1, 2, 3, 1, 3, 4, 2 };
 
-	// Set will contain only unique elements
-	// Iterate over the set and display contents
-	for (int val : my_setOfNum2)
-		std::cout << val << "  ";
-	std::cout << std::endl;
+    // Set will contain only unique elements
+    // Iterate over the set and display contents
+    for (int val : my_setOfNum2)
+        std::cout << val << "  ";
+    std::cout << std::endl;
 
-	std::vector<int> vec( { 14, 5, 6, 7 });
-	std::vector<int> my_vec{ 14, 5, 6, 7 };
+    std::vector<int> vec( { 14, 5, 6, 7 });
+    std::vector<int> my_vec{ 14, 5, 6, 7 };
 
-	// Create an unordered set and initialize it with vector
-	std::unordered_set<int> setOfNum3(vec.begin(), vec.end());
-	std::unordered_set<int> my_setOfNum3{my_vec.begin(), my_vec.end()};
+    // Create an unordered set and initialize it with vector
+    std::unordered_set<int> setOfNum3(vec.begin(), vec.end());
+    std::unordered_set<int> my_setOfNum3{my_vec.begin(), my_vec.end()};
 
-	// Set will contain only unique elements
-	// Iterate over the set and display contents
-	for (int val : my_setOfNum3)
-		std::cout << val << "  ";
-	std::cout << std::endl;
+    // Set will contain only unique elements
+    // Iterate over the set and display contents
+    for (int val : my_setOfNum3)
+        std::cout << val << "  ";
+    std::cout << std::endl;
 
-	return 0;
+
+    // ------------------------------------------------- //
+    // Vector practice                                   //
+    // ------------------------------------------------- //
+
+
+    // An array (aka list in Python) of strings
+    std::vector<string> strings_list{"hello", "world", "test", "123"};
+
+    for (const auto str : strings_list) {
+        std::cout << str << " ";
+    }
+    std::cout << std::endl;
+
+    // int 2d, 3x3 matrix
+    std::cout << "***** 3x3 matrix 2d int vector *****" << std::endl;
+    std::vector<std::vector<int>> vec_2d_int{{1, 2, 3},
+                                            {4, 5, 6},
+                                            {7, 8, 9}};
+
+    for (int i = 0; i < vec_2d_int.size(); i++) {
+        for (int j = 0; j < vec_2d_int[i].size(); j++) {
+            std::cout << vec_2d_int[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    std::vector<std::vector<string>> string_2d{ {"hello1", "world1"},
+                                                {"hello2", "world2"},
+                                                {"hello3", "world3"}};
+
+    for (std::vector<string>& pstr : string_2d) {
+        for (string& str : pstr) {
+            std::cout << str << std::endl;
+        }
+    }
+
+    // Using auto. 'const' means you cannot modify the content of the pointer
+    // Try change and you will face compile error
+    // If you remove 'const', then you can modify
+    for (const auto& pstr : string_2d) {
+        for (const auto& string str : pstr) {
+            std::cout << str << std::endl;
+        }
+    }
+
+
+    return 0;
 }
---
+++ cpp/unord_set_hasher.cc
@@ -0,0 +1,60 @@
+#include <cstdio>
+#include <cstdint>
+#include <iostream>
+#include <string>
+#include <cassert>
+#include <unordered_set>
+#include <algorithm>
+
+using namespace std;
+
+// https://thispointer.com/using-unordered_set-with-custom-hasher-and-comparision-function/
+
+// Custom hasher
+struct StrHashByLength {
+	size_t operator()(const string &str) const {
+		int length = str.length();
+		// TODO: why double parentheses
+		//   https://stackoverflow.com/questions/40024008/how-to-understand-two-pairs-of-parentheses-in-this-code-fragment
+		return hash<int>()(length);
+	}
+};
+
+// Custom Comparator function
+struct StrCompareByLength {
+	// Recall const args vs const member function
+	bool operator()(const string &lhs, const string &rhs) const {
+		if (lhs.length() == rhs.length())
+			return true;
+		else
+			return false;
+	}
+};
+
+int main(int argc, char const *argv[])
+{
+	// NOTE: notice the ordering. Spent quite some time debugging just because
+	//   you put hasher arg as the 3rd args to the template (╯°□°）╯︵ ┻━┻)
+	//   C++ template compile is super cryptic and magical, defeating Perl
+	unordered_set<string, StrHashByLength, StrCompareByLength> setOfStrs;
+
+	setOfStrs.insert("First");
+	setOfStrs.insert("Second");
+
+	// Try to insert element with same length as "First"
+	// It will not be added
+	setOfStrs.insert("third");
+
+	// Try to insert element with same length as "Second"
+	// It will not be added
+
+	setOfStrs.insert("second");
+	setOfStrs.insert("five");
+
+	// Only three elements are added in set
+	// Display them
+	for (std::string s : setOfStrs)
+		std::cout << s << std::endl;
+
+	return 0;
+}
