32d69f6625d2ffb395165ca42b0cc948ad4b8067
banoris <banoris@protonmail.com>
2019-11-21 21:57:23 +0100

    Python: containers, list comprehension

--- python/containers.py
+++ python/containers.py
@@ -13,6 +13,9 @@ dict1 = {'name': 'Ellie', 'type': 'Elephant'}
 for i in dict1:
 	print(i)
 
+for value in dict1.values():
+	print(value)
+
 print("=======================================================")
 
 # Python 3, iteritems is removed
@@ -50,6 +53,14 @@ for animal in animals:
 
 print("=======================================================")
 
+print("Python list comprehension -- remove Ellie from animals list")
+animals = [anim for anim in animals if not anim['name'] == "Ellie"]
+for animal in animals:
+	for key, value in animal.items():
+		print(key, value)
+
+print("=======================================================")
+
 seasons = ["Spring", "Summer", "Fall", "Winter"]
 
 # 0 Spring
@@ -58,3 +69,17 @@ seasons = ["Spring", "Summer", "Fall", "Winter"]
 # 3 Winter
 for index, elem in enumerate(seasons):
 	print(index, elem)
+
+
+print("=======================================================")
+print("Dynamically insert data structure")
+
+farm = []
+
+farm.append({'name': 'nemo', 'type': 'ikan'})
+farm.append({'name': 'dumbo', 'type': 'elephant'})
+
+for stuff in farm:
+	for key, value in stuff.items():
+		print("key={}, value={}".format(key, value))
+
