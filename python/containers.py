8eb15878449efc4d30facbca6eaea15937711518
banoris <banoris@protonmail.com>
2019-12-01 22:56:36 +0100

    Python: more containers operations...

--- python/containers.py
+++ python/containers.py
@@ -1,4 +1,4 @@
-
+from operator import itemgetter
 
 array = [1, 2, 3, "hello"]
 for i in array:
@@ -82,4 +82,44 @@ farm.append({'name': 'dumbo', 'type': 'elephant'})
 for stuff in farm:
 	for key, value in stuff.items():
 		print("key={}, value={}".format(key, value))
+
+print("num of key in farm_dict = %d" % len(farm))
+
+print("=======================================================")
+print("Array to string")
+
+int_list = ["123", "44", "122", "1256", 45, 234]
+int_list.append(1111111111)
+
+print(str(int_list))
+
+
+print("=======================================================")
+print("Sort array of hashes by key")
+
+table = [{'id': 1, 'entry': "entry1"},
+		{'id': 4, 'entry': "entry4"},
+		{'id': 3, 'entry': "entry3"},
+		{'id': 2, 'entry': "entry2"}]
+
+print("Before sort by id")
+for cell in table:
+	print("id={}, entry={}".format(cell['id'], cell['entry']))
+
+print("After sort by id")
+table = sorted(table, key=itemgetter('id'))
+
+for cell in table:
+	print("id={}, entry={}".format(cell['id'], cell['entry']))
+
+
+print("Delete and rearrange id")
+table.pop(1) # delete id=2
+
+for cell in table:
+	if cell['id'] > 1:
+		cell['id'] -= 1
+
+for cell in table:
+	print("id={}, entry={}".format(cell['id'], cell['entry']))
 
