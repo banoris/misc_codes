e90b69606b8c9b47bcfca72c9683978fc9319bc7
Badiuzzaman Iskhandar <banoris@protonmail.com>
2020-12-15 11:26:27 +0100

    Python tuple as dict keys. Order dict based on the tuple fields

--- python/containers.py
+++ python/containers.py
@@ -1,5 +1,6 @@
 from operator import itemgetter
 import functools
+from collections import namedtuple, OrderedDict
 
 array = [1, 2, 3, "hello"]
 for i in array:
@@ -168,3 +169,68 @@ if 0 > 3:
 	print("WTH 0 > 3??")
 else:
 	print("Cool, that works")
+
+
+print("=======================================================")
+print("Tuple as key")
+
+Event = namedtuple("Event", ["node", "clock"])
+
+ev1 = Event(node=1, clock=10)
+ev11 = Event(node=1, clock=20)
+ev12 = Event(node=1, clock=3)
+ev2 = Event(node=2, clock=1)
+ev3 = Event(node=3, clock=1)
+ev5 = Event(node=1, clock=1)
+
+
+board = {ev1: "hello1_1",
+         ev2: "hello2_1",
+         ev3: "hello3_1",
+         # ev5: "hello1_5", # weird bug when sorted?
+         ev11: "hello1_2",
+         ev12: "hello1_3"} # override ev1, since key(ev1) == key(ev5)
+# board = sorted(board, key=itemgetter('node', 'clock')) # ERROR?
+# board2 = sorted(board.items(), key=lambda x:(x[0].clock, x[0].node)) # OK!
+# board = sorted(board, key=lambda x:(x.node, x.clock)) # now board is list?
+# board = sorted(board.items(), key=itemgetter('node', 'clock'))
+
+print("=== Default board, no sorting ===")
+print(board)
+
+# NOTE: sorted() returns a list!! dict aka map is unordered by default
+for key, val in board.items():
+    print(f"key.node={key.node}, key.clock={key.clock}, value={val}")
+
+# by default, sort by first key, i.e. 'node'
+ord_board = OrderedDict(sorted(board.items()))
+print("=== Default sorted, by first key 'node' ===")
+for key, val in ord_board.items():
+    print(f"key.node={key.node}, key.clock={key.clock}, value={val}")
+
+# NOTE: you misunderstood! x[1] is the value of the dict, so you are sorting
+# based on the strings: ["hello1_3", "hello2_1", ...]
+print("=== sort by 'clock' field ===")
+# FIXME: ord_board = OrderedDict(sorted(board.items(), key=lambda x: x[1]))
+ord_board = OrderedDict(sorted(board.items(), key=lambda x: x[0].clock))
+
+for key, val in ord_board.items():
+    print(f"key.node={key.node}, key.clock={key.clock}, value={val}")
+
+print("=== sort by 'node' and then 'clock' ===")
+# E.g., (node=1, clock=30) < (node2, clock=2).
+# If you print, (node=1, clock=30) will appear first
+ord_board = OrderedDict(sorted(board.items(), key=lambda x: (x[0].clock, x[0].node)))
+
+"""
+NOTE: as you can see, (node=2, clock=1) is printed first compared to
+      (node=3, clock=1). Eventhough both have same clock values,
+      node2 printed first since node2 < node3
+=== sort by 'node' and then 'clock' ===
+key.node=2, key.clock=1, value=hello2_1
+key.node=3, key.clock=1, value=hello3_1
+key.node=1, key.clock=3, value=hello1_3
+"""
+for key, val in ord_board.items():
+    print(f"key.node={key.node}, key.clock={key.clock}, value={val}")
+
